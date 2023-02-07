021b44616d70a1ee21be48c608c65c5153a1f0d9
Mateusz Gawroński <gawronski.m91@gmail.com>
2023-02-07 10:53:51 +0100

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,32 @@
+
+def is_palindrome(data):
+    '''funkcja do sprawdzenia'''
+    if data == "":
+        return
+    elif not isinstance(data, str):
+        return False
+    else:
+        lowercase_data = data.lower().replace(" ","")
+
+        s = lowercase_data[::-1]
+        s = s.replace(" ","")
+
+        if lowercase_data == s:
+            return s
+        else:
+            return False
+
+
+test_cases = {
+    "Anna" : "anna",
+    "radar" : "radar",
+    "Mateusz" : False,
+    "" : None,
+    "Mamuta tu mam" : "mamutatumam",
+    22 : False,
+}
+
+for data, expectation in test_cases.items():
+    response = is_palindrome(data)
+    assert response == expectation, \
+        f"{expectation} from {data} nie jest rowne {response}"
